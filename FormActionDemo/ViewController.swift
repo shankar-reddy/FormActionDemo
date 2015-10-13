aa08e6473c628dbee86c097e23399cbb37a8235b
Sankara Telukutla <sankara.telukutla@gmail.com>
2015-10-13 00:06:38 -0700

    Initial files

--- FormActionDemo/ViewController.swift
+++ FormActionDemo/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet  var label: UILabel!
+    @IBOutlet  var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("View just loaded")
+        print(textField.text)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+ 
+    @IBAction func submit(sender: AnyObject) {
+        print(textField.text)
+        label.text = "Hello Mr, "+textField.text!
+        
+    }
+   
 }
 
